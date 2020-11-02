22f185fa1a106c3985bc4c8832be05ec686ace30
NandoAM <fagudom@gmail.com>
2020-11-02 08:38:15 +0100

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+//guadar en el cache dinámico
+function actualizaCacheDinamico(dynamicCache, req, res) {
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+}
